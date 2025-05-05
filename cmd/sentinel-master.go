c9cedb01671914333bb99f0eeda52e9f4b044c1e
Mikolaj Pawlikowski <mikolaj@pawlikowski.pl>
2025-05-05 17:04:06 +0100

    implement subcommand sentinel master

---
+++ cmd/sentinel-master.go
@@ -0,0 +1,50 @@
+package cmd
+
+import (
+	"github.com/redis/go-redis/v9"
+	"github.com/seeker89/redis-resiliency-toolkit/pkg/config"
+	"github.com/seeker89/redis-resiliency-toolkit/pkg/printer"
+	"github.com/seeker89/redis-resiliency-toolkit/pkg/redisClient"
+	"github.com/spf13/cobra"
+)
+
+var sentinelMasterCmd = &cobra.Command{
+	Use:   "master",
+	Short: "Show the details of the redis master",
+	Long:  ``,
+	Run: func(cmd *cobra.Command, args []string) {
+		ExecuteSentinelMasters(&cfg, &redisCfg, prtr)
+	},
+}
+
+func init() {
+	sentinelCmd.AddCommand(sentinelMasterCmd)
+}
+
+func ExecuteSentinelMasters(
+	config *config.RRTConfig,
+	redisConfig *config.RedisConfig,
+	printer *printer.Printer,
+) error {
+	rdb, err := redisClient.MakeRedisClient(redisConfig.SentinelURL)
+	if err != nil {
+		return err
+	}
+	{
+		cmd := redis.NewMapStringStringCmd(ctx, "SENTINEL", "master", redisConfig.SentinelMaster)
+		rdb.Process(ctx, cmd)
+		res, _ := cmd.Result()
+		printer.Print(
+			[]map[string]string{res},
+			[]string{
+				"name",
+				"quorum",
+				"config-epoch",
+				"num-slaves",
+				"port",
+				"ip",
+			},
+		)
+	}
+	return nil
+}

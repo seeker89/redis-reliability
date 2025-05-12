fbc8ccab0ddfc2559a83f766aede81ef4ddbe1a6
Mikolaj Pawlikowski <mikolaj@pawlikowski.pl>
2025-05-12 08:38:56 +0100

    Add the logic to listen in on new master election

--- cmd/sentinel-kill.go
+++ cmd/sentinel-kill.go
@@ -30,10 +30,11 @@ func ExecuteSentinelKill(
 	printer *printer.Printer,
 ) error {
 	// we'll be emitting events one by one
+	printer.SkipHeaders = true
 	printer.Itemise = true
 	printOne := func(data map[string]string) {
 		data["time"] = time.Now().String()
-		printer.Print([]map[string]string{data}, []string{"time", "event", "msg"})
+		printer.Print([]map[string]string{data}, []string{"time", "event", "ch", "msg"})
 	}
 	pq := make(chan map[string]string, 10)
 	go func() {
@@ -45,6 +46,7 @@ func ExecuteSentinelKill(
 	if err != nil {
 		return err
 	}
+	done := make(chan error)
 	// The plan here is:
 	// 1. read the master from sentinel
 	// 2. query INFO from the master to see that it matches what sentinel gave us
@@ -56,14 +58,76 @@ func ExecuteSentinelKill(
 	// 6. wait for either 1) timeout, or 2) +switch-master event
 	// 7. read the master from sentinel again
 	// 8. query INFO from the master again
-	masterStart, err := redisClient.GetMasterFromSentinel(rdbs, ctx, redisConfig.SentinelMaster)
+	oldMaster, err := redisClient.GetMasterFromSentinel(rdbs, ctx, redisConfig.SentinelMaster)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		return err
 	}
 	pq <- map[string]string{
 		"event": "initial master",
-		"msg":   fmt.Sprintf("%s:%s", masterStart.Host, masterStart.Port),
+		"msg":   fmt.Sprintf("%s:%s", oldMaster.Host, oldMaster.Port),
 	}
-	return nil
+	// listen in on new sentinel events
+	spubsub := rdbs.PSubscribe(ctx, "+*")
+	defer spubsub.Close()
+	go func() {
+		for msg := range spubsub.Channel() {
+			switch msg.Channel {
+			case "+switch-master":
+				evt, err := redisClient.ParseSwitchMasterMessage(msg.Payload)
+				if err != nil {
+					pq <- map[string]string{
+						"event": "bad message",
+						"msg":   err.Error(),
+					}
+					done <- err
+					continue
+				}
+				// ignore if the message for different master
+				if evt.Master != redisConfig.SentinelMaster {
+					pq <- map[string]string{
+						"event":  "different master",
+						"master": evt.Master,
+					}
+					continue
+				}
+				// final check
+				if evt.OldMasterHost != oldMaster.Host || evt.OldMasterPort != oldMaster.Port {
+					done <- fmt.Errorf("previous master doesn't match; got %v, wanted %v", evt, oldMaster)
+					break
+				}
+				newMaster, err := redisClient.GetMasterFromSentinel(rdbs, ctx, redisConfig.SentinelMaster)
+				if err != nil {
+					done <- err
+					break
+				}
+				if evt.NewMasterHost != newMaster.Host || evt.NewMasterPort != newMaster.Port {
+					done <- fmt.Errorf("new master doesn't match; got %v, wanted %v", newMaster, evt)
+					break
+				}
+				pq <- map[string]string{
+					"event":      "done",
+					"result":     "OK",
+					"new_master": fmt.Sprintf("%s:%s", evt.NewMasterHost, evt.NewMasterPort),
+				}
+				done <- nil
+			}
+			pq <- map[string]string{
+				"event": "sentinel message",
+				"ch":    msg.Channel,
+				"msg":   msg.Payload,
+			}
+		}
+	}()
+	// add a max timeout for all of this
+	go func() {
+		timeout := 60 * time.Second
+		time.Sleep(timeout)
+		pq <- map[string]string{
+			"event":    "timeout",
+			"duration": timeout.String(),
+		}
+		done <- fmt.Errorf("timeout after %s", timeout)
+	}()
+	return <-done
 }
--- pkg/redisClient/redis.go
+++ pkg/redisClient/redis.go
@@ -2,6 +2,8 @@ package redisClient
 
 import (
 	"context"
+	"fmt"
+	"strings"
 
 	"github.com/redis/go-redis/v9"
 )
@@ -9,6 +11,14 @@ import (
 type RedisInstance struct {
 	Host string
 	Port string
+}
+
+type RedisSwitchMasterEvent struct {
+	Master        string
+	NewMasterHost string
+	NewMasterPort string
+	OldMasterHost string
+	OldMasterPort string
 }
 
 func MakeRedisClient(url string) (*redis.Client, error) {
@@ -31,5 +41,19 @@ func GetMasterFromSentinel(rdb *redis.Client, ctx context.Context, master string
 	return &RedisInstance{
 		Host: res[0],
 		Port: res[1],
+	}, nil
+}
+
+func ParseSwitchMasterMessage(msg string) (*RedisSwitchMasterEvent, error) {
+	parts := strings.Split(msg, " ")
+	if len(parts) != 5 {
+		return nil, fmt.Errorf("expected formatted redis +switch-master, got %s", msg)
+	}
+	return &RedisSwitchMasterEvent{
+		Master:        parts[0],
+		OldMasterHost: parts[1],
+		OldMasterPort: parts[2],
+		NewMasterHost: parts[3],
+		NewMasterPort: parts[4],
 	}, nil
 }
